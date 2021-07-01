1d5c3a5f443be3e04e219e8345ac40a61aca5172
Mumtaz Bano <mumtazbano@Mumtazs-MBP.lan>
2021-06-30 19:52:37 -0500

    Commiting changes

--- src/backend/api_app.py
+++ src/backend/api_app.py
@@ -1,7 +1,3 @@
-#import flask as Flask
-#from flask_restful import Resource, Api, reqparse
-#from src.backend.classifier import Classifier
-
 
 import pathlib
 import os
@@ -9,47 +5,19 @@ from flask import Flask, render_template, request, redirect, url_for, abort,flas
 from werkzeug.utils import secure_filename
 
 
-#app = Flask(__name__)
-#api = API(app)
-
-"""class RalphLaurenFeedPredicitons(Resource):
-	def get(self):
-		# Will eventually make batch predictions
-		pass
-
-
-#api.add_resource(RalphLaurenFeedPredictions, 'ralph_lauren_feed_predict')
-
-"""
-
-
-
-
-
-UPLOAD_FOLDER = '/Users/mumtazbano/GLP-Group-1-21/UploadsFolder'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
+
+
+
+UPLOAD_FOLDER = 'static/uploads/'
+app.secret_key = "secret key"
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/uploader', methods = ['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and ALLOWED_EXTENSIONS(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-
-
-if __name__ == '__main__':
-	app.run()
+    file = request.files["file"]
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return "File Successfully Uploaded"
 

43901fb8d7fa6950892f645a74573a3e202f7a41
Mumtaz Bano <mumtazbano@Mumtazs-MBP.lan>
2021-06-30 21:00:41 -0500

    Adding changes

--- src/backend/api_app.py
+++ src/backend/api_app.py
@@ -1,7 +1,5 @@
-
-import pathlib
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort,flash
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 
 
@@ -14,10 +12,33 @@ app.secret_key = "secret key"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+# Allowed file type extensions
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
+    
     file = request.files["file"]
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return "File Successfully Uploaded"
+
+
+    if file.filename == '':
+        print('No image selected for uploading')
+    
+    if file and allowed_file(file.filename):
+
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "File Uploaded Successfully"
+    
+    else:
+        print('Allowed file types are - png, jpg, jpeg, gif')
+        
 
